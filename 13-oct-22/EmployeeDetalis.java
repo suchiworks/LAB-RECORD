f24ea0fa58205f4fef1f5daf4038fef92776f4bc
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-10-21 10:48:57 +0530

    Add files via upload

---
+++ 13-oct-22/EmployeeDetalis.java
@@ -0,0 +1,23 @@
+package prac13thOct;
+/*write a java class to develop an employee class 
+ * object & initialized the emp name & emp id for 5 emp. now write 
+ * another class having name and from this class you have 
+ * to create 5 employee objects but you are not allowed to 
+ * use new keyword*/
+public class EmployeeDetalis {
+
+	int id;
+	String name;
+	public class EmployeeDetalis("int id" , "String name") {
+	this.id=id;
+	this.name=name;
+		
+}
+void show() {
+	System.out.println(id+" "+name);
+	System.out.println(name);
+
+}}
+public static void main (String[] args) {
+	
+}
