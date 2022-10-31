5c5d78a6ce8d445c24fe308b2477709f33f27c7e
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-10-31 13:50:17 +0530

    Array's 1D

---
+++ 31-oct-22/Anonymous.java
@@ -0,0 +1,14 @@
+package lab31oct;
+
+public class Anonymous {
+
+	 static void print(int[] arr) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<arr.length;i++); 
+		System.out.println(arr[i]);
+
+	
+public static void main(String[] args) {
+	print(new int[] {10,20,30,40,50,60,70,80});
+}
+}
