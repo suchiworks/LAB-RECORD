b27e22870d66912d10993c8a0abe7e954daa697b
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-10-31 13:54:52 +0530

    Update Anonymous.java

--- 31-oct-22/Anonymous.java
+++ 31-oct-22/Anonymous.java
@@ -2,13 +2,13 @@ package lab31oct;
 
 public class Anonymous {
 
-	 static void print(int[] arr) {
-		// TODO Auto-generated method stub
-		for(int i=0; i<arr.length;i++); 
-		System.out.println(arr[i]);
-
-	
-public static void main(String[] args) {
-	print(new int[] {10,20,30,40,50,60,70,80});
-}
+	static void print(int arr[]) { 
+		//static print methods store and print array
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+	public static void main(String[] args) {
+		print(new int[] {10,20,30,40,50}); 
+		//passing random values in the print method's array
+	}
 }
