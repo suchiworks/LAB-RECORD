86d59f6a060eb64d8548d96866152e376262482c
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-11-03 13:24:12 +0530

    Update OddPosition.java

--- 3-nov-22/OddPosition.java
+++ 3-nov-22/OddPosition.java
@@ -1,10 +1,12 @@
 package lab3Nov;
 
+//the elements present on odd positions are a, c and e.
+
 public class OddPosition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 int [] arr = new int [] {1, 2, 3, 4, 5};   
+		 int [] arr = new int [] {5,6,7,8,9};   
 System.out.println("Elements of given array present on "
 		                               + "odd position: ");  
 
