d0ee4ebec997d0961353407f65819eb4a7f3c3a6
suchiworks <111557185+suchiworks@users.noreply.github.com>
2022-11-03 13:23:13 +0530

    Update DuplicateValue.java

--- 3-nov-22/DuplicateValue.java
+++ 3-nov-22/DuplicateValue.java
@@ -1,16 +1,16 @@
 package lab3Nov;
 
+//Write a Java program to find the duplicate values of an array of integer values.
+
+
 public class DuplicateValue {
 		public static void main(String[] args) 
 	    {
-	        int[] my_array = {1, 2, 5, 5, 6, 6, 7, 2};
-	 
-	        for (int i = 0; i < my_array.length-1; i++)
-	        {
-	            for (int j = i+1; j < my_array.length; j++)
-	            {
-	                if ((my_array[i] == my_array[j]) && (i != j))
-	                {
-	                    System.out.println("Duplicate Element : "+my_array[j]);
+	        int[] a = {5,2,7,7,5};	 
+	    for (int i = 0; i < a.length-1; i++)	        
+	        for (int j = i+1; j < a.length; j++) {{
+	            
+                 if ((a[i] ==a[j]) && (i != j)) {        
+ System.out.println("Duplicate Element : "+a[j]);
 	       }}}}}
 	
